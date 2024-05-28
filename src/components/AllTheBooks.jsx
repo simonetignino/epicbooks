e4858ffb9a9cc8d404e8d92fcddd543e24768109
simonetignino <simo7820@gmail.com>
2024-05-28 16:50:59 +0200

    sistemata grafica delle card

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -5,10 +5,10 @@ export default function AllTheBooks(props) {
     console.log(props.books);
   return (
     <Row>
-      {props.books(book =>(
+      {props.books.map(book =>(
           <Col>
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={book.img} />
+          <Card style={{ height: '550px', width: '300px', margin:'10px 0'}}>
+            <Card.Img variant="top" src={book.img} style={{height: '400px'}} />
             <Card.Body>
               <Card.Title>{book.title}</Card.Title>
             </Card.Body>
